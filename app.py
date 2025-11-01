71b734e8941409d0bf86ebd4657d71cdbf20cba7
soumyahacker <pg2196577@gmail.com>
2025-11-02 03:01:36 +0530

    second commit

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def home():
 @app.route('/magazine/')
 def magazine():
     return render_template('magazine/index.html')
+# Add to app.py
+@app.route('/achievers/')
+def achievers():
+    return render_template('achievers.html')
+@app.route('/faculty/')
+def faculty():
+    return render_template('faculty.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
